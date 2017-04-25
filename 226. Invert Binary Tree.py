90e7a331ce77a2fdb68bcc88371dda454c7abd3a
余果 <fishguo321@gmail.com>
2017-04-25 19:24:36 +0800

    Create 226. Invert Binary Tree.py

---
+++ 226. Invert Binary Tree.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def invertTree(self, root):
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
