5c0a9092c365a5ec3efbf26e88f9778107bd7b7b
余果 <fishguo321@gmail.com>
2017-04-25 23:52:15 +0800

    Update 563. Binary Tree Tilt.py

--- 563. Binary Tree Tilt.py
+++ 563. Binary Tree Tilt.py
@@ -10,3 +10,19 @@ class Solution(object):
             return root.val + self.calTotal(root.left) + self.calTotal(root.right)
         else:
             return 0
+
+ # 优化后的代码：只递归单次
+
+class Solution(object):
+    def findTilt(self, root):
+        diff = [0]
+        def dfs(root):
+            if root:
+                left = dfs(root.left)
+                right = dfs(root.right)
+                diff[0] += abs(left - right)
+                return left + right + root.val
+            else:
+                return 0
+        dfs(root)
+        return diff[0]
