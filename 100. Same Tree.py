a07a7dbe5680f69ecd6384132e344ed45a41748e
余果 <fishguo321@gmail.com>
2017-04-26 00:02:57 +0800

    Update 100. Same Tree.py

--- 100. Same Tree.py
+++ 100. Same Tree.py
@@ -6,3 +6,11 @@ class Solution(object):
             return True
         else:
             return False
+
+ # 优化后的代码：更简洁
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p == q
